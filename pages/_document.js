3acd5276341fd9472bebecc440d08945461f7f4a
Colo <colo@MBP-de-Colo.fibertel.com.ar>
2020-06-03 21:10:54 -0300

    Adding Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              <!-- Global site tag (gtag.js) - Google Analytics -->
+              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168587762-1"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'UA-168587762-1');
+              </script>
+                `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
